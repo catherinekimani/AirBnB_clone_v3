25437cb1e179dee5a3bab5931807662d0e9579ca
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2023-10-30 23:22:06 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,18 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    return jsonify({"status": "OK"})
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    stats_obj = {
+        'states': State, 'users': User,
+        'amenities': Amenity, 'cities': City,
+        'places': Place, 'reviews': Review
+    }
+    for key in stats_obj:
+        stats_obj[key] = storage.count(stats_obj[key])
+    return jsonify(stats_obj)
