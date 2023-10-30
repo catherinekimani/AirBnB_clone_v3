2411d6b34bf73aef605578640920d42120aeb0d1
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-30 19:53:26 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,9 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
+from flask import jsonify
+from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,12 +17,19 @@ def teardown(exception):
     """ Close current session """
     storage.close()
 
-@app.errorhandler
-def page_not_found():
-    return ({'error': 'Not found'}), 404
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
