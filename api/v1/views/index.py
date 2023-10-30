c5e1210f432727da0a947ac66bb0bf37d908f74f
catherinekimani <catherinekimani882@gmail.com>
2023-10-30 23:49:51 +0300

    Fix typo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,4 +8,19 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """ Return Status code """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """Retrieves the no of each object by type"""
+    objects = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
+    }
+    return jsonify(objects)
