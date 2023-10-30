6704acf5007b33083cf9bd69469e6c13a91ec5ec
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2023-10-30 23:19:01 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,15 +9,27 @@ from os import getenv
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def downtear(self):
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({'error': 'Not found'}), 404
+
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
     app.run(host=host, port=port, threaded=True)
