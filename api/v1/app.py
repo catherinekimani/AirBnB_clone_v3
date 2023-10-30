a1768ca1d1af8df5878c015336cc50e8637653e2
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2023-10-30 23:10:56 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,27 +9,14 @@ from os import getenv
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
-    """ Close current session """
     storage.close()
 
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify({'error': 'Not found'}), 404
-
-
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    else:
-        port = 5000
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
