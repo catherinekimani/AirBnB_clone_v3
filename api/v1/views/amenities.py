a13739e4bae48568ca55a3d58971d507cbe73f3f
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-10-30 21:53:06 +0000

    amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -14,7 +14,8 @@ def get_amenities():
     return jsonify(amenities)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET']
+                 strict_slashes=False)
 def get_amenity_by_id(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
@@ -22,7 +23,8 @@ def get_amenity_by_id(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE']
+                 strict_slashes=False)
 def delete_amenities(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
@@ -46,7 +48,8 @@ def create_amenities():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT']
+                 strict_slashes=False)
 def update_amenities(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
